734d269aa46d005b0fbed85f368dfa627ae317cc
Coddo <40121850+Coddo-Python@users.noreply.github.com>
2023-06-24 20:30:25 +0400

    Get Authentication Working

--- src/saladpy/client.py
+++ src/saladpy/client.py
@@ -7,79 +7,139 @@ from aiohttp import ClientSession
 from .exceptions import *
 from .methods import Methods
 
+
 class SaladClient(Methods):
-    API_URL = 'https://app-api.salad.io/api/v1/'
-    AUTH_API_URL = 'https://app-api.salad.io/api/v2/'
-    
-    def __init__(self, accessToken: Optional[str] = None, cachePath: Optional[Path] = None, *args, **kwargs):
-        self.cookies =  {"sAccessToken": accessToken} if accessToken else {}
+    BASE_API_URL = "https://app-api.salad.com"
+    V1_API_URL = "https://app-api.salad.com/api/v1/"
+    V2_API_URL = "https://app-api.salad.io/api/v2/"
+    AUTH_API_URL = "https://app-api.salad.com/auth/"
+    _DEBUG_PROXY = "http://localhost:8000"
+
+    def __init__(
+        self,
+        accessToken: Optional[str] = None,
+        cachePath: Optional[Path] = None,
+        *args,
+        **kwargs,
+    ):
+        self.cookies = {"sAccessToken": accessToken} if accessToken else {}
         self.cachePath = cachePath
+        self.cached = False
         self.http = ClientSession(cookies=self.cookies, *args, **kwargs)
-    
+        if self.cachePath is not None:
+            if self.cachePath.exists():
+                if self.cachePath.stat().st_size > 0:  # File is not empty
+                    self.http.cookie_jar.load(self.cachePath)
+                    self.cached = True
+                            
+    async def close(self):
+        """
+        Closes aiohttp `ClientSession` safely
+        Using any methods after calling `close` will not work
+        """
+        await self.http.close()
+
     async def login(self, email: str):
-        headers = {'Content-Type': 'application/json'}
-        payload = json.dumps({"email":email,"termsAccepted": True})
-        async with self.http.post(f"https://app-api.salad.com/api/v2/authentication-sessions", data=payload, headers=headers) as r:
-            try:
-                assert r.status == 204
-                # print("resp cookie")
-                # print(r.headers)
-                # print("------------------")
-                # print(r.cookies)
-                # print("------------------")
-                # # self.http.cookie_jar.update_cookies()
-                # print(self.http.cookie_jar.filter_cookies('app-api.salad.com'))
-                # print("---------------------")
-                # print(list(self.http.cookie_jar))
-            except AssertionError:
-                text = await r.text()
-                raise SaladPyException(f"Error {r.status} during email verification! {text}")
-            else: 
-                return self._verify
-    
+        """Gets the necessary authentication tokens required for API requests
+
+        Args:
+            email (str): Salad E-Mail
+
+        Raises:
+            SaladPyException: An error during Email Verification
+
+        Returns:
+            Callable: A function representing the `_verify` method, takes in otp as argument
+        """
+        if not self.cached:
+            headers = {"Content-Type": "application/json"}
+            payload = json.dumps({"email": email, "termsAccepted": True})
+            async with self.http.post(
+                f"{self.V2_API_URL}authentication-sessions",
+                data=payload,
+                headers=headers, proxy=self._DEBUG_PROXY
+            ) as r:
+                try:
+                    assert r.status == 204
+                    # Ensure that the `sInitToken` cookie is correctly applied to the session
+                    # If not done correctly, OTP verification will not work
+                    self.http.cookie_jar.update_cookies(r.cookies)
+                except AssertionError:
+                    text = await r.text()
+                    raise SaladPyException(
+                        f"Error {r.status} during email verification! {text}"
+                    )
+                else:
+                    return self._verify
+
     async def _verify(self, otp: str):
-        headers = {'Content-Type': 'application/json'}
-        payload = json.dumps({"passcode": str(otp)})
-        async with self.http.post(f"https://app-api.salad.com/api/v2/authentication-sessions/verification", data=payload, headers=headers) as r:
-            try:
-                assert r.status == 204
-                cookies = list(self.http.cookie_jar)
-                print(cookies)
-                print("-------------------------")
-                print("Sucessfully logged in!")
-                if self.cachePath is not None:
-                    self.cachePath.touch(exist_ok=True)
-                    with self.cachePath.open(mode="w") as f:
-                        tokens = {cookie.key: cookie.value for cookie in cookies }
-                        json.dump(tokens, f)
-            except AssertionError:
-                text = await r.json()
-                raise SaladPyException(f"Error {r.status} during OTP verification! {text}")
-            else: 
-                return True
-            
-        
+        """Verify using OTP sent to Salad E-Mail
+
+        Args:
+            otp (str): The OTP given by the user
+
+        Raises:
+            SaladPyException: An error during OTP verification
+
+        Returns:
+            Optional[Bool]: True if no errors occured
+        """
+        if not self.cached:
+            headers = {
+                "Content-Type": "application/json",
+            }
+            payload = {"passcode": str(otp)}
+            cookies = {cookie.key: cookie.value for cookie in list(self.http.cookie_jar) }
+            async with self.http.post(
+                f"{self.V2_API_URL}authentication-sessions/verification",
+                json=payload,
+                headers=headers,
+                cookies=cookies,
+            ) as r:
+                try:
+                    cookies = list(self.http.cookie_jar)
+                    assert r.status == 204
+                    self.http.cookie_jar.update_cookies(r.cookies)
+                    if self.cachePath is not None:
+                        self.cachePath.touch(exist_ok=True)
+                        self.http.cookie_jar.save(self.cachePath)
+                except AssertionError:
+                    text = await r.json()
+                    raise SaladPyException(
+                        f"Error {r.status} during OTP verification! {text}"
+                    )
+                else:
+                    return True
+
     async def _req(self, method: str, endpoint: str, params: Optional[dict] = None):
+        cookies = {cookie.key: cookie.value for cookie in list(self.http.cookie_jar) }
         async with self.http.request(
-            method, f"{self.API_URL}{endpoint}", params=params
+            method, f"{self.V1_API_URL}{endpoint}", params=params, cookies=self.cookies, proxy=self._DEBUG_PROXY
         ) as r:
-            json = await r.text()
+            text = await r.text()
             try:
-                print(json)
                 assert r.status == 200
             except AssertionError:
-                print("bro it errored out")
-                print(f"json here: {json}")
-                print(f"status; {r.status}")
-                # raise SaladPyException(
-                #     json
-                # )
+                if (
+                    "try refresh token" in text
+                ):  # DO NOT convert to JSON as it's not always guaranteed to be JSON
+                    # Refresh token
+                    print("Token expired, refreshing...")
+                    await self.refresh_token()
             else:
-                return json
+                return await r.json()
 
+    async def refresh_token(self):
+        cookies = {cookie.key: cookie.value for cookie in list(self.http.cookie_jar) }
+        async with self.http.post(f"{self.AUTH_API_URL}session/refresh", cookies=cookies, proxy=self._DEBUG_PROXY) as r:
+            print(r.status)
+            text = await r.text()
+            print(text)
+            self.http.cookie_jar.update_cookies(r.cookies)
+            self.http.cookie_jar.save(self.cachePath)
 
     async def _get(self, endpoint: str, params: Optional[dict] = None):
         return await self._req("GET", endpoint, params)
-    
+
     async def _post(self, endpoint: str, params: Optional[dict] = None):
         return await self._req("POST", endpoint, params)
