faf43bd954ee73adb740074b13365d9823e7b68d
Coddo <40121850+Coddo-Python@users.noreply.github.com>
2023-07-09 17:12:06 +0400

    Change Version Tag

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Issue tracker': 'https://github.com/Coddo-Python/SaladPy/issues',
         'Docs': 'https://saladpy.gitbook.io/saladpy-docs/'
     },
-    version="1.0.2-a.2",
+    version="1.0.2-a.3",
     license='MIT',
     description='A Python wrapper for the Salad Web API',
     long_description=readme,
